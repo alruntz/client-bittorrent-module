3676a7cb8b8a84ca19c937f0ae35923ce5b20aa8
alruntz <alruntz@student.le-101.fr>
2018-11-04 05:26:24 +0100

    last commit

---
+++ test/main.js
@@ -0,0 +1,11 @@
+var should = require('should');
+var torrentStream = require('../lib/main');
+
+/*describe('torrentStream', function() {
+		it('return new client', function(done) {
+				torrentStream('./test/test2.torrent');
+				done();
+		});*/
+
+torrentStream('./test/test2.torrent');
+
